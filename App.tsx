f3c96845cd933a1214ea2e69c563eb6d363fdd60
diogoX451 <diogo.g07@icloud.com>
2022-10-24 12:51:16 -0300

    main

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
 
+import * as Location from 'expo-location';
 export default function App() {
+  const [location, setLocation] = useState();
+  const [errorMsg, setErrorMsg] = useState();
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let { latitude, longitude } = '';
+  
+  if (errorMsg) {
+    console.log(errorMsg);
+  } else if (location) {
+    latitude = location.coords.latitude;
+    longitude = location.coords.longitude;
+    console.log(latitude);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Localização Atual é: </Text>
+      <Text>{latitude}</Text>
+      <Text>{longitude}</Text>
       <StatusBar style="auto" />
     </View>
   );
